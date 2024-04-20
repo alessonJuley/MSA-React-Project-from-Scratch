1a27bb69d5484c9bee2a49888927183d42dba779
alessonJuley <alessonabao@gmail.com>
2024-04-21 03:06:19 +1200

    Added test page

---
+++ react-recipes/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import PageTitle from './PageTitle';
+import './index.css'
+
+function App() {
+    return (
+        <article>
+            <h1>MSA Week 2</h1>
+            <PageTitle/>
+        </article>
+    )
+}
+
+export default App;
---
+++ react-recipes/src/PageTitle.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function PageTitle() {
+    const title = 'Hello, my name is Alesson.';
+    return (
+        <h2>{ title }</h2>
+    )
+};
+
+export default PageTitle;
